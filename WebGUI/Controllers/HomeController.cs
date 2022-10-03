b7a40b177e59f75ffa2293ac6c53e5fc835289d8
Shashen-PC <shashengg@gmail.com>
2022-10-03 07:16:39 +0530

    Implemented web gui

---
+++ WebGUI/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace WebGUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Home";
+
+            RestClient restClient = new RestClient("http://localhost:52604/");
+            RestRequest request = new RestRequest("api/getalldata");
+            RestResponse resp = restClient.Get(request);
+
+            List<BankData> data = JsonConvert.DeserializeObject<List<BankData>>(resp.Content);
+
+            return View(data);
+        }
+    }
+}
---
+++ WebGUI/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace WebGUI.Models
+{
+    public class BankData
+    {
+        public int Id { get; set; }
+
+        public string FName { get; set; }
+
+        public string LName { get; set; }
+
+        public int AcctNo { get; set; }
+
+        public int Pin { get; set; }
+
+        public int Balance { get; set; }
+
+        public string Image { get; set; }
+
+    }
+}
